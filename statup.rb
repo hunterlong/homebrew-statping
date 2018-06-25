d38b0ef6c849e3769b9cbd9c72f820dd1ce53267
Hunter Long <info@socialeck.com>
2018-06-25 08:59:46 -0700

    new

--- statup.rb
+++ statup.rb
@@ -4,20 +4,20 @@ class Statup < Formula
   homepage "https://github.com/hunterlong/statup"
   version "0.241"
   if OS.mac?
-    if bits_64?
+    if Hardware.is_64_bit?
       url "https://github.com/hunterlong/statup/releases/download/0.241/statup-osx-x64.tar.gz"
-      sha256 "7da1b8641b93eb10f1d20e3b72362e7aa817eb9d4ec88767b2dfddbfcf445f20"
+      sha256 "15525a130db2d164180dc9f0ed16d3c6a4abf330fdc63cc7d43dea4834447cbf"
     else
       url "https://github.com/hunterlong/statup/releases/download/0.241/statup-osx-x32.tar.gz"
-      sha256 "f3c40d2f7deb689433d22d73b99cb029999023451f9f7f5f89c5c7139e542278"
+      sha256 "ac5940aaefa43236671588bc275ebb4e0c92a60cf316ccd307e4230a93323967"
     end
   elsif OS.linux?
-      if bits_64?
+      if Hardware.is_64_bit?
         url "https://github.com/hunterlong/statup/releases/download/0.241/statup-linux-x64.tar.gz"
-        sha256 "ffd5b337c4bf27f214b86bc7e36bdb9877bc2b7c8103f2230aa653b492c088fe"
+        sha256 "089619ee19af2f67beba1ab0fcc3863e79105e335d88bf17d6ab8efd2b4d3696"
       else
         url "https://github.com/hunterlong/statup/releases/download/0.241/statup-linux-x32.tar.gz"
-        sha256 "4962542c0a83df1d7498586635a8ddb366d5eea47da844d0f392e33258605833"
+        sha256 "9a191a3a0c39ac049e1140527d1684587d92704bf35dc28be424974831707313"
       end
   end
 
@@ -29,7 +29,4 @@ class Statup < Formula
     system "#{bin}/statup", "version"
   end
 
-  def bits_64?
-    Hardware.is_64_bit?
-  end
 end
