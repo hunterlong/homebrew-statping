eb8ec7d4cbdbe83890a92728c4bbbbcf59844302
Hunter Long <info@socialeck.com>
2018-06-25 09:17:04 -0700

    new

--- build.sh
+++ build.sh
@@ -3,23 +3,32 @@
 VERSION=0.241
 REPO=https://github.com/hunterlong/statup/releases/download
 
+rm -rf build
 mkdir build
 
 BINFILE=statup-osx-x64.tar.gz
-curl -o build/$BINFILE $REPO/v$VERSION/$BINFILE
-OSX64=`openssl dgst -sha256 build/$BINFILE | sed 's/^.*= //'`
+curl -o build/$BINFILE -OL $REPO/v$VERSION/$BINFILE
+tar -xvzf build/$BINFILE
+OSX64=`openssl dgst -sha256 statup | sed 's/^.*= //'`
+rm -f statup
 
 BINFILE=statup-osx-x32.tar.gz
-curl -o build/$BINFILE $REPO/v$VERSION/$BINFILE
-OSX32=`openssl dgst -sha256 build/$BINFILE | sed 's/^.*= //'`
+curl -o build/$BINFILE -OL $REPO/v$VERSION/$BINFILE
+tar -xvzf build/$BINFILE
+OSX32=`openssl dgst -sha256 statup | sed 's/^.*= //'`
+rm -f statup
 
 BINFILE=statup-linux-x64.tar.gz
-curl -o build/$BINFILE $REPO/v$VERSION/$BINFILE
-LIN64=`openssl dgst -sha256 build/$BINFILE | sed 's/^.*= //'`
+curl -o build/$BINFILE -OL $REPO/v$VERSION/$BINFILE
+tar -xvzf build/$BINFILE
+LIN64=`openssl dgst -sha256 statup | sed 's/^.*= //'`
+rm -f statup
 
 BINFILE=statup-linux-x32.tar.gz
-curl -o build/$BINFILE $REPO/v$VERSION/$BINFILE
-LIN32=`openssl dgst -sha256 build/$BINFILE | sed 's/^.*= //'`
+curl -o build/$BINFILE -OL $REPO/v$VERSION/$BINFILE
+tar -xvzf build/$BINFILE
+LIN32=`openssl dgst -sha256 statup | sed 's/^.*= //'`
+rm -f statup
 
 cat formula | \
     sed -e "s/\$VERSION/$VERSION/" \
--- statup.rb
+++ statup.rb
@@ -6,18 +6,18 @@ class Statup < Formula
   if OS.mac?
     if Hardware::CPU.is_64_bit?
       url "https://github.com/hunterlong/statup/releases/download/v0.241/statup-osx-x64.tar.gz"
-      sha256 "92d55a7d70e3d7dec8a465eb8111d8ee8bdcbc5c4b509d131084bf1db6ab2ead"
+      sha256 "c47417b0159a856eac6d9192b73ac073e77677ee2997513981ddea6f256a0b5d"
     else
       url "https://github.com/hunterlong/statup/releases/download/v0.241/statup-osx-x32.tar.gz"
-      sha256 "1944dd011c074d7e7f9c6ebfababeb30b32bd3885812d8fc9a94eb96bad42f9a"
+      sha256 "24757b6d2bed1682d5620cf9af97e46ca83a70f5fdbb5634f4233257af61bfb3"
     end
   elsif OS.linux?
       if Hardware::CPU.is_64_bit?
         url "https://github.com/hunterlong/statup/releases/download/v0.241/statup-linux-x64.tar.gz"
-        sha256 "22aecef1f5016b11717a1dc37c63abd8eef0caae4e338a2438bb44368785e6e8"
+        sha256 "a83c0bca33b8a765988456957d34edc88544cc9eb92a29e2fa6103f89563ea85"
       else
         url "https://github.com/hunterlong/statup/releases/download/v0.241/statup-linux-x32.tar.gz"
-        sha256 "299176a6d22242f48a05a4363c302ff1e7d0d31104880905de66fb0705f2171f"
+        sha256 "c98f417f43c4bc647955f8855a95ced186e50864a20fd84eb3dcf36ea3a26abc"
       end
   end
 
