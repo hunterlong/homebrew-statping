166cc0aaac148c0ada36abd37b1bdac4b877438d
Hunter Long <Info@socialeck.com>
2018-06-24 23:43:12 -0700

    brew

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+VERSION=v0.23
+
+REPO=https://github.com/hunterlong/statup/releases/download
+
+mkdir build
+
+BINFILE=statup-osx-x64
+curl -o build/$BINFILE $REPO/$VERSION/$BINFILE
+OSX64=`openssl dgst -sha256 build/$BINFILE | sed 's/^.*= //'`
+
+BINFILE=statup-osx-x32
+curl -o build/$BINFILE $REPO/$VERSION/$BINFILE
+OSX32=`openssl dgst -sha256 build/$BINFILE | sed 's/^.*= //'`
+
+BINFILE=statup-linux-x64
+curl -o build/$BINFILE $REPO/$VERSION/$BINFILE
+LIN64=`openssl dgst -sha256 build/$BINFILE | sed 's/^.*= //'`
+
+BINFILE=statup-linux-x32
+curl -o build/$BINFILE $REPO/$VERSION/$BINFILE
+LIN32=`openssl dgst -sha256 build/$BINFILE | sed 's/^.*= //'`
+
+cat formula | \
+    sed -e "s/\$VERSION/$VERSION/" \
+    -e "s/\$OSX64/$OSX64/" \
+    -e "s/\$OSX32/$OSX32/" \
+    -e "s/\$LIN64/$LIN64/" \
+    -e "s/\$LIN32/$LIN32/" \
+    > statup.rb
+
+git add statup.rb
+git commit -m "Statup $VERSION"
+git push origin master
