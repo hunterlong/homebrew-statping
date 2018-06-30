70dba06478261636c1bd18fa455af6388e279cde
Hunter Long <Info@socialeck.com>
2018-06-29 23:54:19 -0700

    brew

--- statup.rb
+++ statup.rb
@@ -2,21 +2,21 @@ class Statup < Formula
   homepage 'https://statup.io'
   desc "An easy to use Status Page for your websites and applications."
   homepage "https://github.com/hunterlong/statup"
-  version ""
+  version "0.27.9"
   if OS.mac?
     if bits_64?
-      url "https://github.com/hunterlong/statup/releases/download//statup-osx-x64.tar.gz"
+      url "https://github.com/hunterlong/statup/releases/download/0.27.9/statup-osx-x64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/hunterlong/statup/releases/download//statup-osx-x32.tar.gz"
+      url "https://github.com/hunterlong/statup/releases/download/0.27.9/statup-osx-x32.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   elsif OS.linux?
       if bits_64?
-        url "https://github.com/hunterlong/statup/releases/download//statup-linux-x64.tar.gz"
+        url "https://github.com/hunterlong/statup/releases/download/0.27.9/statup-linux-x64.tar.gz"
         sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
       else
-        url "https://github.com/hunterlong/statup/releases/download//statup-linux-x32.tar.gz"
+        url "https://github.com/hunterlong/statup/releases/download/0.27.9/statup-linux-x32.tar.gz"
         sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
       end
   end
