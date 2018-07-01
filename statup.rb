e7e33f5a66374026ceb6abfa7f1173acbe073bcd
hunterlong <info@socialeck.com>
2018-06-30 21:33:45 -0700

    Release to v0.28.4

--- statup.rb
+++ statup.rb
@@ -2,22 +2,22 @@ class Statup < Formula
   homepage 'https://statup.io'
   desc "An easy to use Status Page for your websites and applications."
   homepage "https://github.com/hunterlong/statup"
-  version "0.27.9"
+  version "0.28.4"
   if OS.mac?
-    if bits_64?
-      url "https://github.com/hunterlong/statup/releases/download/0.27.9/statup-osx-x64.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    if Hardware::CPU.is_64_bit?
+      url "https://github.com/hunterlong/statup/releases/download/v0.28.4/statup-osx-x64.tar.gz"
+      sha256 "731ebbe60865ebdc2104ae1bf18f73445d88a55e1410133aedeac3a51171f16a"
     else
-      url "https://github.com/hunterlong/statup/releases/download/0.27.9/statup-osx-x32.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/hunterlong/statup/releases/download/v0.28.4/statup-osx-x32.tar.gz"
+      sha256 "3c8b29dfc2e30478a10c2f2bce13c3f6d480da2a4dfe427c9ea9dcc0aa03b649"
     end
   elsif OS.linux?
-      if bits_64?
-        url "https://github.com/hunterlong/statup/releases/download/0.27.9/statup-linux-x64.tar.gz"
-        sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/hunterlong/statup/releases/download/v0.28.4/statup-linux-x64.tar.gz"
+        sha256 "55eeb60bb4ac354af10fd10381705ff87ff03c22a84cff24bed761467f4e88da"
       else
-        url "https://github.com/hunterlong/statup/releases/download/0.27.9/statup-linux-x32.tar.gz"
-        sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+        url "https://github.com/hunterlong/statup/releases/download/v0.28.4/statup-linux-x32.tar.gz"
+        sha256 "badf40c3d90028760ac06295b48f99a5f1090ef748a7debac4cdc4d345eb5d50"
       end
   end
 
@@ -29,7 +29,4 @@ class Statup < Formula
     system "#{bin}/statup", "version"
   end
 
-  def bits_64?
-    Hardware.is_64_bit?
-  end
 end
