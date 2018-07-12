dba3f2af75b95e9f2d24ac8bf44aaea61ede03a3
hunterlong <info@socialeck.com>
2018-07-12 19:39:29 +0000

    New Homebrew v0.29.9 132a173

--- statup.rb
+++ statup.rb
@@ -5,18 +5,18 @@ class Statup < Formula
   if OS.mac?
     if Hardware::CPU.is_64_bit?
       url "https://github.com/hunterlong/statup/releases/download/v0.29.9/statup-osx-x64.tar.gz"
-      sha256 "942af6eacfc3a9028840c8c533518d34ad849522b26a1a6c4fe803a00a202612"
+      sha256 "8c4777e07885c59680cf226dd1eadba56f912fcb7b5575850b703ab687e87c58"
     else
       url "https://github.com/hunterlong/statup/releases/download/v0.29.9/statup-osx-x32.tar.gz"
-      sha256 "21f38e59b7f143f8f6b84c7694aa87983ca7952d25d054d17ac7dadc0a54a5fc"
+      sha256 "78576b6b609dd9262c69ad3a8a4eddb42b2a53c46eb7e0c5d18d30f05f9be3ec"
     end
   elsif OS.linux?
     if Hardware::CPU.is_64_bit?
       url "https://github.com/hunterlong/statup/releases/download/v0.29.9/statup-linux-x64.tar.gz"
-      sha256 "7a65c66270ba8c91b1bb1f0084e93179745c3f32c537c260e2582106603edb0f"
+      sha256 "461befd314a1d02d15ece6f654ca550cd971415be1ba75e4f98180254f8f1eeb"
     else
       url "https://github.com/hunterlong/statup/releases/download/v0.29.9/statup-linux-x32.tar.gz"
-      sha256 "98b1734f4832df1b39b589b5f3500ce8de6cf01784977a526246f1b45ccf8226"
+      sha256 "3ace6c697b6f75fa473e6cddea54d892f08e4fc4f2f3bf9281ce283d284e381c"
     end
   end
 
