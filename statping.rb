763ef3d2f4f4dac5d47f002413307ca9e11c8ab6
hunterlong <info@socialeck.com>
2019-02-01 21:02:51 +0000

    New Homebrew v0.80.43 1175e83

--- statping.rb
+++ statping.rb
@@ -1,22 +1,22 @@
 class Statping < Formula
   desc "Track and monitor your websites and applications uptime"
   homepage "https://github.com/hunterlong/statping"
-  version "0.80.42"
+  version "0.80.43"
   if OS.mac?
     if Hardware::CPU.is_64_bit?
-      url "https://github.com/hunterlong/statping/releases/download/v0.80.42/statping-osx-x64.tar.gz"
-      sha256 "c1334e3f4ac2e1fbe6628437174877e3ee1c995e37f1d0eec2ea974bb346a9d6"
+      url "https://github.com/hunterlong/statping/releases/download/v0.80.43/statping-osx-x64.tar.gz"
+      sha256 "691eec4eb7d2256a7cdc8b25460adc79e237427ebaacab86a8d65deb7ae815a5"
     else
-      url "https://github.com/hunterlong/statping/releases/download/v0.80.42/statping-osx-x32.tar.gz"
-      sha256 "e353510af5b303d8821b0999051fc296bebbf4f2fbd2e6729ca462bb8615b9d7"
+      url "https://github.com/hunterlong/statping/releases/download/v0.80.43/statping-osx-x32.tar.gz"
+      sha256 "b931b682dddbac372c909ddbbd0ffc097d840b2ac9d946c252cedb284aa1227b"
     end
   elsif OS.linux?
     if Hardware::CPU.is_64_bit?
-      url "https://github.com/hunterlong/statping/releases/download/v0.80.42/statping-linux-x64.tar.gz"
-      sha256 "4860496d7e0ae3b0472cb662e621ec1926cdcc6bcb8dd33d588640c44c419879"
+      url "https://github.com/hunterlong/statping/releases/download/v0.80.43/statping-linux-x64.tar.gz"
+      sha256 "a90c6151d382814d7500731023a8828d9cc88ad6a1eb643ffef3ae86f9a00b27"
     else
-      url "https://github.com/hunterlong/statping/releases/download/v0.80.42/statping-linux-x32.tar.gz"
-      sha256 "6e9578e69d63bac86cc3415bccd7cb14e664c69de50d59ca0a0dc2897b84e708"
+      url "https://github.com/hunterlong/statping/releases/download/v0.80.43/statping-linux-x32.tar.gz"
+      sha256 "fc7d671c7f57a9f8af3238fd0adf7046e5cb6d0ec576fb895402ac79187c5cb9"
     end
   end
 
