ba6b6b43ede0d0bb8e1ab2dc0e0cfac10fd52d2c
hunterlong <info@socialeck.com>
2020-03-24 18:37:23 +0000

    New Homebrew v0.90.16 c23ed22

--- statping.rb
+++ statping.rb
@@ -1,22 +1,22 @@
 class Statping < Formula
   desc "Track and monitor your websites and applications uptime"
   homepage "https://github.com/statping/statping"
-  version "0.90.15"
+  version "0.90.16"
   if OS.mac?
     if Hardware::CPU.is_64_bit?
-      url "https://github.com/statping/statping/releases/download/v0.90.15/statping-osx-x64.tar.gz"
-      sha256 "e37f4578c0cf293197b7b6d13ed372aa7d2b3585174ca5ca9057900a3c72a0b4"
+      url "https://github.com/statping/statping/releases/download/v0.90.16/statping-osx-x64.tar.gz"
+      sha256 "3eded8125a384acd9cd8646a3b2d3dc2b6148b47a688cc8a6e68d47eb094d9d0"
     else
-      url "https://github.com/statping/statping/releases/download/v0.90.15/statping-osx-x32.tar.gz"
-      sha256 "960199bd981f95999a002b7d2777ad50ec2632650730ce936fcf6b317a775a5a"
+      url "https://github.com/statping/statping/releases/download/v0.90.16/statping-osx-x32.tar.gz"
+      sha256 "abc95356adaf1735032c4156b175c46729699cccf4b915454709f721eebfa695"
     end
   elsif OS.linux?
     if Hardware::CPU.is_64_bit?
-      url "https://github.com/statping/statping/releases/download/v0.90.15/statping-linux-x64.tar.gz"
-      sha256 "d042c6367d7c4711cecbeadb2201d7e6bc370abae53108a3fa661d068fa9d6bf"
+      url "https://github.com/statping/statping/releases/download/v0.90.16/statping-linux-x64.tar.gz"
+      sha256 "6021826fb32b6e94d864c31cc7a476cda4d05bb5a4f5949197daa85b4c8b7a1d"
     else
-      url "https://github.com/statping/statping/releases/download/v0.90.15/statping-linux-x32.tar.gz"
-      sha256 "1ead2b18104da8fd5a28018f544be832e9b12a97f4f9ef8e73e55c19e43ce8cf"
+      url "https://github.com/statping/statping/releases/download/v0.90.16/statping-linux-x32.tar.gz"
+      sha256 "8c01f87ae2d0c983cddd228651398adadb10185fc09b99d7f458e7d4084dd788"
     end
   end
 
